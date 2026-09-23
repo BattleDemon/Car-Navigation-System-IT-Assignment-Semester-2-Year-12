dd8d99e85c42978dedc92c16d0d4adc904515cfc
DexterH <DexterHart0@outlook.com>
2026-09-23 15:02:56 +1000

    added the imports for the pathfindi9ng library to the navigation controller file

--- Code/navigation/navigation.py
+++ Code/navigation/navigation.py
@@ -1,3 +1,8 @@
+from pathfinding.core.graph import graph
+from pathfinding.finder.a_star import AStar
+from pathfinding.core.diagonal_movement import DiagonalMovement
+
+
 class NavigationController:
     def __init__(self):
         pass
